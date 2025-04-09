59f4748a04ab4a7a10e11d9dab30c5032961fa08
Michael <200~pregnant-mutt-cape@duck.com>
2025-04-09 18:00:47 -0500

    Improved functionality, now does empty strings!

--- recursive1.js
+++ recursive1.js
@@ -1,10 +1,13 @@
 function reverseString(str) {
-    if(str.length === 1) {
+    if(str.length === 1 || 0 ) { // Base statement
         return str;
-    }   
-    return str.charAt(str.length -1) + reverseString(str.slice(0, -1));
+    }
+    return str.charAt(str.length - 1) + reverseString(str.slice(0, -1)); // returns last letter, then splice last letter from string and run again until string = 1 or 0
 }
 
-console.log(reverseString("man"));
-console.log(reverseString("Ghost"));
-console.log(reverseString("Muckduck"));
+
+ console.log(reverseString("Hello"));
+ console.log(reverseString("Church"));
+ console.log(reverseString("MuckDuck"));
+ console.log(reverseString(""));
+ console.log(reverseString("Bro"));
