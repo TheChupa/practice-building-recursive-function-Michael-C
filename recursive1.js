2f630398fc4744c2f297926023f613b3c5f444aa
Michael <200~pregnant-mutt-cape@duck.com>
2025-04-09 18:43:17 -0500

    last fix

--- recursive1.js
+++ recursive1.js
@@ -1,13 +1,13 @@
 function reverseString(str) {
-    if(str.length === 1 || 0 ) { // Base statement
+    if(str.length === 1 || str.length === 0) { // Base statement
         return str;
     }
-    return str.charAt(str.length - 1) + reverseString(str.slice(0, -1)); // returns last letter, then splice last letter from string and run again until string = 1 or 0
+    return str.charAt(str.length - 1) + reverseString(str.slice(0, -1)); // 
 }
 
 
- console.log(reverseString("Hello"));
- console.log(reverseString("Church"));
- console.log(reverseString("MuckDuck"));
- console.log(reverseString(""));
- console.log(reverseString("Bro"));
+ console.log(reverseString("Hello")); // elloH
+ console.log(reverseString("Church")); // hcruhC
+ console.log(reverseString("MuckDuck")); // kcuDkcuM
+ console.log(reverseString("")); // ""
+ console.log(reverseString("Bro")); // orB
